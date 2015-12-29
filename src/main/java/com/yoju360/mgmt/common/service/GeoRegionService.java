bef64da1c1ee32c7a88f5e29ee99b204bfc4edbc
vic.huang <Administrator@PC201511231622>
2015-12-29 14:12:12 +0800

    extract GeoRegionService interface

--- src/main/java/com/yoju360/mgmt/common/service/GeoRegionService.java
+++ src/main/java/com/yoju360/mgmt/common/service/GeoRegionService.java
@@ -3,46 +3,16 @@ package com.yoju360.mgmt.common.service;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.yoju360.mgmt.common.mapper.GeoRegionMapper;
 import com.yoju360.mgmt.common.model.GeoRegion;
 import com.yoju360.mgmt.common.model.GeoRegionExample;
-import com.yoju360.mgmt.core.mapper.BaseMapper;
-import com.yoju360.mgmt.core.service.AbstractCoreService;
+import com.yoju360.mgmt.core.service.CoreService;
 
-@Service("geoRegionService")
-@Transactional
-public class GeoRegionService extends AbstractCoreService<GeoRegion, GeoRegionExample> {
-	@Autowired
-	private GeoRegionMapper geoRegionMapper;
-	
-	@Override
-	protected BaseMapper<GeoRegion, GeoRegionExample> getMapper() {
-		return geoRegionMapper;
-	}
+public interface GeoRegionService extends CoreService<GeoRegion, GeoRegionExample> {
+	List<Map<String, Object>> getProvinces();
 
-	@Override
-	public String getModuleName() {
-		return null;
-	}
+	List<Map<String, Object>> getCities(Long provinceId);
 
-	public List<Map<String, Object>> getProvinces() {
-		return geoRegionMapper.getProvinces();
-	}
+	List<Map<String, Object>> getRegions(Long cityId);
 
-	public List<Map<String, Object>> getCities(Long provinceId) {
-		return geoRegionMapper.getCities(provinceId);
-	}
-
-	public List<Map<String, Object>> getRegions(Long cityId) {
-		return geoRegionMapper.getRegions(cityId);
-	}
-
-	public String getFullyAreaPrefix(Long provinceId, Long cityId, Long regionId) {
-		return getModel(provinceId).getName() + getModel(cityId).getName() + getModel(regionId).getName();
-	}
-
+	String getFullyAreaPrefix(Long provinceId, Long cityId, Long regionId);
 }
